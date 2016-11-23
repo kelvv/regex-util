68fce1ed0c59ca704262e5af4052d01a4144919c
kelvv <kelvv@outlook.com>
2016-11-23 16:57:05 +0800

    :bug: fix global method

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const checker = require('./libs/checker');
+const matcher = require('../libs/matcher');
 /**
  * Export the root object
  */
@@ -10,3 +11,8 @@ function _regex() {}
 for(var method of Object.keys(checker)){
     _regex[method] = checker[method]
 }
+
+for(var method of Object.keys(matcher)){
+    _regex[method] = matcher[method]
+}
+
